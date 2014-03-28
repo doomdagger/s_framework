a50d6e4ebdbab7947c28597f40539dc9790285da
He Li <jet.in.brain@gmail.com>
2014-03-28 15:32:57 +0800

    save EntityGenerator first

--- src/main/java/com/hg/ecommerce/model/support/EntityGenerator.java
+++ src/main/java/com/hg/ecommerce/model/support/EntityGenerator.java
@@ -1,5 +1,22 @@
 package com.hg.ecommerce.model.support;
 
-public class EntityGenerator {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import com.hg.ecommerce.config.ProjectConfig;
+
+public class EntityGenerator {
+	private Connection connection;
+	{
+		String url = ProjectConfig.getProperty("db.url");
+		String username = ProjectConfig.getProperty("db.username");
+		String password = ProjectConfig.getProperty("db.password");
+		try{
+			connection = DriverManager.getConnection(url, username, password);
+			
+		}catch(SQLException exception){
+			
+		}
+	}
 }
