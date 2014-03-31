d037a1c556b062e2de9600387257e92f41744b48
He Li <jet.in.brain@gmail.com>
2014-03-31 18:27:30 +0800

    bug fixed:you can inject jdbcTemplate now!

--- src/main/java/com/hg/ecommerce/dao/impl/BaseDaoImpl.java
+++ src/main/java/com/hg/ecommerce/dao/impl/BaseDaoImpl.java
@@ -21,14 +21,6 @@ public class BaseDaoImpl<T> implements BaseDao<T>{
 	
 	private JdbcTemplate jdbcTemplate;
 	
-	public JdbcTemplate getJdbcTemplate() {
-		return jdbcTemplate;
-	}
-	
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-	
 	
 	@SuppressWarnings("unchecked")
 	public BaseDaoImpl(){
