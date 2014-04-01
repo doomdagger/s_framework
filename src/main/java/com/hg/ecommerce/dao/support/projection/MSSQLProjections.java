c06223d20b8d4cb21e8518e82381019edbae5364
joechow <787449527@qq.com>
2014-04-01 16:21:01 +0800

    add projection classes, and modify config

---
+++ src/main/java/com/hg/ecommerce/dao/support/projection/MSSQLProjections.java
@@ -0,0 +1,123 @@
+package com.hg.ecommerce.dao.support.projection;
+
+import java.util.Collection;
+
+import com.hg.ecommerce.dao.support.IProjections;
+
+public class MSSQLProjections implements IProjections {
+
+	@Override
+	public IProjections fields(Object... objects) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections fields(Collection<Object> objects) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections AVG(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections COUNT(String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections COUNT(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections FIRST(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections LAST(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections MAX(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections MIN(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections SUM(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections GROUPBY(String field) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections UCASE(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections LCASE(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections MID(String field, long start, long end, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections LEN(String field, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections ROUND(String field, int decimals, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections NOW(String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IProjections FORMAT(String field, String format, String alias) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProjection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
